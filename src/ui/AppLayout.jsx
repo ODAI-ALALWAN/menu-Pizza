992ff7337e62d95617645a9306fcd7ec6c36d605
ODAI ALALWAN <odaialalwan@alMacBook-Air-alkhas-bODAI.local>
2023-10-30 10:20:24 +0300

    Update One

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -12,12 +12,12 @@ function AppLayout() {
 
   return (
     <>
-    <Header />
-    <div className='container' >
+    <div className='container grid h-screen grid-rows-[auto_auto_1fr] ' >
         {isLoading && <Loader />}
-        <div className="overflow-y-scroll">
-          <main className="">
-          <CartOverview/>
+        <Header />
+        <CartOverview/>
+        <div className='overflow-y-scroll' >
+          <main className="mx-auto max-w-3xl">
           <Outlet />
           </main>
         </div>
